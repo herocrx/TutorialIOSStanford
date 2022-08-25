23ab16f84f37b21c42f9017474f7bc5c631c6c13
Hubert Kuc <h_kuc@apple.com>
2022-08-25 10:34:54 +0200

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by Hubert Kuc on 25.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
