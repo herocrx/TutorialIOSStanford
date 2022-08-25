ed094d017fb266348e3d8b57075eebacc8f15455
Hubert Kuc <h_kuc@apple.com>
2022-08-25 17:42:43 +0200

    Class 2

--- Tutorial/ContentView.swift
+++ Tutorial/ContentView.swift
@@ -8,15 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = ["🏎","🚕","🚙","🚖", "🚴‍♀️", "🚓", "✈️", "🚘", "🛩", "🚤", "🚢"]
+    @State var emojiCount = 10
+    
+     
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 230))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji, isFaceUp: true).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            Spacer()
+            HStack {
+                add
+                Spacer()
+                remove
+
+            }.padding(.horizontal, 100)
         }
-        .padding()
     }
+    
+    var remove : some View {
+        Button {
+            emojiCount = max(0, emojiCount - 1)
+        } label: {
+            VStack {
+                Text("Remove")
+                Text("Emoji")
+            }.font(.largeTitle).padding(.horizontal)
+        }
+    }
+    
+    var add : some View {
+        Button {
+            emojiCount = min(emojiCount + 1, emojis.count - 1)
+        } label: {
+            VStack {
+                Text("Add")
+                Text("Emoji")
+            }.font(.largeTitle)
+        }
+    }
+}
+
+struct CardView : View {
+    let content : String
+    @State var isFaceUp : Bool // any let in the struct cannot change, it is not a boolean but it is a POINTER to the boolean!!!!!!
+    // temporary state, in the middle of the drag
+    // the ui is constanty rebuilt!
+    // logic decided how it should look like!
+    
+    
+    var body : some View {
+        ZStack { // <--- content arguemnt to the ZStack
+            let shape = RoundedRectangle(cornerRadius: Constants.roundedRectangleSize); // zstack can have the variables conditions!
+            //let shape = Circle()
+            if isFaceUp {
+                shape.fill(.white).padding(25.0)
+                shape.strokeBorder(lineWidth: 20).padding(25.0).foregroundColor(.red)
+                Text(content).font(.system(size:100))
+            }
+            else {
+                shape.fill(.red).padding(25.0)
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+    
+    struct Constants {
+        static let roundedRectangleSize : CGFloat = 25.0
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
